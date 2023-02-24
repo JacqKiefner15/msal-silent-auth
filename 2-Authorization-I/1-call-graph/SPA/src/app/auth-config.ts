c9f2ca7ce8f96374e37d5f3f82967177e5df7a62
JACQUELYN KIEFNER <jacquelyn.r.kiefner@pwc.com>
2023-02-24 10:03:59 -0700

    get profile photo from graph api

--- 2-Authorization-I/1-call-graph/SPA/src/app/auth-config.ts
+++ 2-Authorization-I/1-call-graph/SPA/src/app/auth-config.ts
@@ -5,15 +5,16 @@
  * in app.module.ts file.
  */
 
+import { UrlSerializer } from '@angular/router';
 import {
-    LogLevel,
-    Configuration,
-    BrowserCacheLocation,
+  LogLevel,
+  Configuration,
+  BrowserCacheLocation,
 } from '@azure/msal-browser';
 
 const isIE =
-    window.navigator.userAgent.indexOf('MSIE ') > -1 ||
-    window.navigator.userAgent.indexOf('Trident/') > -1;
+  window.navigator.userAgent.indexOf('MSIE ') > -1 ||
+  window.navigator.userAgent.indexOf('Trident/') > -1;
 
 /**
  * Configuration object to be passed to MSAL instance on creation.
@@ -22,26 +23,27 @@ const isIE =
  */
 
 export const msalConfig: Configuration = {
-    auth: {
-        clientId: 'Enter_the_Application_Id_Here', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://login.microsoftonline.com/Enter_the_Tenant_Info_Here', // Defaults to "https://login.microsoftonline.com/common"
-        redirectUri: '/auth', // Points to window.location.origin by default. You must register this URI on Azure portal/App Registration.
-        postLogoutRedirectUri: '/', // Points to window.location.origin by default.
-        clientCapabilities: ['CP1'], // This lets the resource server know that this client can handle claim challenges.
+  auth: {
+    clientId: '0d667b5f-1b65-4477-a500-57cf81a6c5ac', // This is the ONLY mandatory field that you need to supply.
+    authority:
+      'https://login.microsoftonline.com/03e832ce-094d-4423-b864-c11e2a626509', // Defaults to "https://login.microsoftonline.com/common"
+    redirectUri: '/auth', // Points to window.location.origin by default. You must register this URI on Azure portal/App Registration.
+    postLogoutRedirectUri: '/', // Points to window.location.origin by default.
+    clientCapabilities: ['CP1'], // This lets the resource server know that this client can handle claim challenges.
+  },
+  cache: {
+    cacheLocation: BrowserCacheLocation.LocalStorage, // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+    storeAuthStateInCookie: isIE, // Set this to "true" if you are having issues on IE11 or Edge. Remove this line to use Angular Universal
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback(logLevel: LogLevel, message: string) {
+        console.log(message);
+      },
+      logLevel: LogLevel.Verbose,
+      piiLoggingEnabled: false,
     },
-    cache: {
-        cacheLocation: BrowserCacheLocation.LocalStorage, // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
-        storeAuthStateInCookie: isIE, // Set this to "true" if you are having issues on IE11 or Edge. Remove this line to use Angular Universal
-    },
-    system: {
-        loggerOptions: {
-            loggerCallback(logLevel: LogLevel, message: string) {
-                console.log(message);
-            },
-            logLevel: LogLevel.Verbose,
-            piiLoggingEnabled: false,
-        },
-    },
+  },
 };
 
 /**
@@ -49,14 +51,18 @@ export const msalConfig: Configuration = {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
 export const protectedResources = {
-    graphMe: {
+  graphMe: {
+    endpoint: 'https://graph.microsoft.com/v1.0/me',
+    scopes: ['User.Read'],
+  },
+  graphContacts: {
+    endpoint: 'https://graph.microsoft.com/v1.0/me/contacts',
+    scopes: ['Contacts.Read'],
+    },
+    graphAvatar: {
         endpoint: 'https://graph.microsoft.com/v1.0/me',
-        scopes: ['User.Read'],
-    },
-    graphContacts: {
-        endpoint: 'https://graph.microsoft.com/v1.0/me/contacts',
-        scopes: ['Contacts.Read'],
-    },
+        scopes: ['User.Read','Contacts.Read'],
+  }
 };
 
 /**
@@ -66,5 +72,5 @@ export const protectedResources = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: ['User.Read', 'Contacts.Read'],
+  scopes: ['User.Read', 'Contacts.Read'],
 };
--- 2-Authorization-I/1-call-graph/SPA/src/app/profile/profile.component.ts
+++ 2-Authorization-I/1-call-graph/SPA/src/app/profile/profile.component.ts
@@ -6,54 +6,68 @@ import { ResponseType } from '@microsoft/microsoft-graph-client';
 import { GraphService, ProviderOptions } from '../graph.service';
 import { protectedResources } from '../auth-config';
 import { Profile } from '../profile';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.css'],
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-    profile!: Profile;
-    displayedColumns: string[] = ['claim', 'value'];
-    dataSource: any = [];
+  profile!: Profile;
+  displayedColumns: string[] = ['claim', 'value'];
+  dataSource: any = [];
+  avatar: any;
+    image: any;
+ 
 
-    constructor(
-        private graphService: GraphService,
-        private authService: MsalService
-    ) { }
+  constructor(
+    private graphService: GraphService,
+    private authService: MsalService,
+    private sanitizer: DomSanitizer
+  ) {}
 
-    ngOnInit() {
-        const providerOptions: ProviderOptions = {
-            account: this.authService.instance.getActiveAccount()!,
-            scopes: protectedResources.graphMe.scopes,
-            interactionType: InteractionType.Redirect,
-            endpoint: protectedResources.graphMe.endpoint,
-        };
+  ngOnInit() {
+    const providerOptions: ProviderOptions = {
+      account: this.authService.instance.getActiveAccount()!,
+      scopes: protectedResources.graphMe.scopes,
+      interactionType: InteractionType.Redirect,
+      endpoint: protectedResources.graphMe.endpoint,
+    };
 
-        this.getProfile(providerOptions);
-    }
+    this.getAvatar(providerOptions);
+  }
 
-    getProfile(providerOptions: ProviderOptions) {
-        this.graphService
-            .getGraphClient(providerOptions)
-            .api('/me')
-            .responseType(ResponseType.RAW)
-            .get()
-            .then((response: any) => {
-                if (response.status === 200) return response.json();
-                if (response.status === 401) {
-                    if (response.headers.get('WWW-Authenticate')) {
-                        this.graphService.handleClaimsChallenge(response, providerOptions);
-                    }
-                }
-            })
-            .then((profileResponse: Profile) => {
-                Object.entries(profileResponse).forEach((claim: [string, unknown]) => {
-                    this.dataSource = [...this.dataSource, { claim: claim[0], value: claim[1] }];
-                });
-            })
-            .catch((error: any) => {
-                console.log(error);
-            });
-    }
+  transform(html: any) {
+    return this.sanitizer.bypassSecurityTrustUrl(html);
+  }
+
+  getAvatar(providerOptions: ProviderOptions) {
+    this.graphService
+      .getGraphClient(providerOptions)
+      .api('/me/photo//$value')
+      .responseType(ResponseType.RAW)
+      .get()
+      .then((response: any) => {
+        if (response.status === 200) {
+             response.blob().then((data: any) => {
+               if (data !== null) {
+                 window.URL = window.URL || window.webkitURL;
+                 console.log('data', data);
+                 var objectUrl = window.URL.createObjectURL(data);
+                   console.log('image', this.image);
+                   this.image = this.sanitizer.bypassSecurityTrustUrl(objectUrl);
+                   
+               }
+             });
+        }
+        if (response.status === 401) {
+          if (response.headers.get('WWW-Authenticate')) {
+            this.graphService.handleClaimsChallenge(response, providerOptions);
+          }
+        }
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  }
 }
